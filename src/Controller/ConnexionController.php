9c510d5d5f8aec94b486eb820521e1ef6fe61c09
ldufaut2020 <laura.dufaut2020@campus-eni.fr>
2021-07-07 09:18:46 +0200

    Création ConnexionController, AppAuthenticator et twig pour connexion

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/", name="connexion_identification")
+     */
+    public function identification(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+             return $this->redirectToRoute('connexion_identification');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('connexion/identification.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+
+    }
+}
